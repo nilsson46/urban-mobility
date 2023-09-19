d2638b3d600e61618e92b7c56ff3e471364aad90
nilsson46 <nilssonsimon46@gmail.com>
2023-09-19 12:03:41 +0200

    Added a test to create a user

--- demo/src/test/java/service/AccountServiceTest.java
+++ demo/src/test/java/service/AccountServiceTest.java
@@ -2,13 +2,19 @@ package service;
 
 import entity.Account;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import repository.AccountRepository;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.*;
+
+
 @ExtendWith(MockitoExtension.class)
 class AccountServiceTest {
 
@@ -31,5 +37,16 @@ class AccountServiceTest {
                 .paymentHistory(0)
                 .activeOrders(0)
                 .build();
+    }
+
+    @Test
+    public void Should_ReturnAccountDetails_When_CreateAccount(){
+        given(accountRepository.save(account)).willReturn(account);
+
+        Account savedAccount = accountService.createAccount(account);
+
+        assertThat(savedAccount).isNull();
+        verify(accountRepository, times(1)).save(account);
+        verifyNoMoreInteractions(accountRepository);
     }
 }
